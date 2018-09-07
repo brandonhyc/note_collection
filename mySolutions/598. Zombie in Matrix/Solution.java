9edac210ed6bbbbffa1c7cd437f94ee933939ef9
Brandonhyc <brandon.heyc@gmail.com>
2018-09-07 15:15:08 -0400

    no.598 (lint)

---
+++ mySolutions/598. Zombie in Matrix/Solution.java
@@ -0,0 +1,44 @@
+public class Solution {
+    public int zombie(int[][] grid) { 
+
+        if (grid.length == 0) return -1;
+        int height = grid.length; int width = grid[0].length; int days = 0;
+
+        Queue<Integer> queX = new LinkedList<>();
+        Queue<Integer> queY = new LinkedList<>();
+        
+        int[] dx = {1, 0, -1, 0}; int[] dy = {0, 1, 0, -1};
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (grid[y][x] == 1) {
+                    queX.offer(x); queY.offer(y);
+                }
+            }
+        }
+
+        while (!queX.isEmpty()) {
+            int x = queX.poll(); int y = queY.poll();
+            days++;
+
+            for (int d = 0; d < dx.length; d++) {
+                if (x + dx[d] >= 0 && x + dx[d] <= width - 1 &&
+                    y + dy[d] >= 0 && y + dy[d] <= height - 1) {
+                    if (grid[y + dy[d]][x + dx[d]] == 0) {
+                        grid[y + dy[d]][x + dx[d]] = 1;
+                        queX.offer(x + dx[d]); 
+                        queY.offer(y + dy[d]);
+                    }
+                }
+            }
+        }
+
+        for (int[] y: grid) {
+            for (int x: y) {
+                if (x == 0) return -1;
+            }
+        }
+        
+        return days;
+    }
+}
