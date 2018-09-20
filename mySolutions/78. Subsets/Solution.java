1655881d53a90396bbece1a0e4f36c77198629b3
Brandonhyc <brandon.heyc@gmail.com>
2018-09-20 10:17:25 -0400

    subset 2nd

--- mySolutions/78. Subsets/Solution.java
+++ mySolutions/78. Subsets/Solution.java
@@ -3,6 +3,8 @@ public class Solution {
      * @param nums: A set of numbers
      * @return: A list of lists
      */
+
+
     public List<List<Integer>> subsets(int[] nums) {
         // write your code here
         
@@ -35,5 +37,40 @@ public class Solution {
             }
         }
         return result;
+    }
+}
+
+//  DFS
+public class Solution { 
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (nums.length == 0) {
+            result.add(new ArrayList<>());
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        dfs(nums, result, 0, new ArrayList<Integer>());
+        return result;
+    }
+
+    private void dfs(int[] nums, List result, int index, List subset) {
+
+        if (index == nums.length) {
+            result.add(subset);
+            return;
+        }
+
+        List<Integer> one = new ArrayList<>(subset);
+
+        one.add(nums[index]);
+
+        dfs(nums, result, index + 1, one);
+
+        dfs(nums, result, index + 1, subset);
+        return;
     }
 }
