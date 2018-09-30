56104841ab825322d8ba9c75841b376f50bbda1d
Brandonhyc <brandon.heyc@gmail.com>
2018-09-30 13:45:49 -0400

    no.290

---
+++ mySolutions/290. Word Pattern/Solution.java
@@ -0,0 +1,31 @@
+public class Solution {
+    /**
+     * @param pattern: a string, denote pattern string
+     * @param teststr: a string, denote matching string
+     * @return: an boolean, denote whether the pattern string and the matching
+     *          string match or not
+     */
+    public boolean wordPattern(String pattern, String teststr) {
+        // write your code here
+        Map<Character, String> map = new HashMap<Character, String>();
+        String[] arrOfStr = teststr.split(" ");
+
+        if (teststr.length() == 0 || pattern.length() == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < pattern.length(); i++) {
+
+            if (!map.containsKey(pattern.charAt(i))) { 
+                if (map.containsValue(arrOfStr[i])) {
+                    return false; 
+                }
+                map.put(pattern.charAt(i), arrOfStr[i]);
+            }
+            if (map.get(pattern.charAt(i)) != arrOfStr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
