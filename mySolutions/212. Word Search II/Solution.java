8d3e6569d005877609fdfd740a2163305d585fd0
Brandonhyc <brandon.heyc@gmail.com>
2018-10-02 16:41:20 -0400

    no.212

--- mySolutions/212. Word Search II/Solution.java
+++ mySolutions/212. Word Search II/Solution.java
@@ -1,3 +1,10 @@
+// 第一步是取每一个单词的前序组成一个map<String, Boolean>. map中对应的单词, 为true, 其余为false
+// 进行dfs. 每次dfs进行对一次临近4个元素的bfs.
+//  剪枝: 若超出范围, 退出. 若不存在前序, 退出. 
+// 难点: 太难. 没法说
+// 易错点: 出口选错. 可能存在一种情况是单词1是单词2的前序, 因此不可以提早结束dfs.
+
+
 public class Solution {
     /**
      * @param board: A list of lists of character
@@ -11,25 +18,41 @@ public class Solution {
     public List<String> wordSearchII(char[][] board, List<String> words) {
         // write your code here
 
-        List<String> result = new ArrayList<String>();
+        Set<String> result = new HashSet<String>();
         if (board == null || board.length == 0) {
-            return result;
+            return new ArrayList<>(result);
         }
 
-        int width = board.length;
-        int height = board[0].length;
         Map<String, Boolean> prefixIsWord = getPrefixSet(words);
-        boolean[][] visited = new boolean[height][width]; 
+        boolean[][] visited = new boolean[board.length][board[0].length]; 
         
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
                 visited[i][j] = true;
-                dfs();
+                dfs(board, result, prefixIsWord, "" + board[i][j], i, j, visited);
                 visited[i][j] = false;
             }
         }
+        return new ArrayList<String>(result);
     }
-    private void dfs (char[][] board, List<String> result,
+
+    private Map<String, Boolean> getPrefixSet(List<String> words) {
+        
+        Map<String, Boolean> map = new HashMap<>();
+        for (String word : words) {
+            String prefix = "";
+            for (char c : word.toCharArray()) {
+                prefix += c;
+                if (!map.containsKey(prefix)) {
+                    map.put(prefix, false);
+                }
+            }
+            map.put(word, true);
+        }
+        return map;
+    }
+
+    private void dfs (char[][] board, Set<String> result,
                         Map<String, Boolean> prefixIsWord, 
                         String word, 
                         int x, int y, boolean[][] visited) {
@@ -37,19 +60,15 @@ public class Solution {
             return;
         }
 
-        if (prefixIsWord(word)) {
+        if (prefixIsWord.get(word)) {
             result.add(word);
-            return;
         }
 
         for (int i = 0; i < 4; i++) {
             int adjX = x + dx[i];
             int adjY = y + dy[i];
 
-            if (visited[adjX][adjY]) {
-                continue;
-            }
-            if (!inside(board, adjX, adjY)) {
+            if (!inside(board, adjX, adjY) || visited[adjX][adjY]) {
                 continue;
             }
 
@@ -63,10 +82,7 @@ public class Solution {
     }
 
     private boolean inside(char[][] board, int x, int y) {
-        return x >= 0 && x < board.length && y >= 0 && y < board[0].length;
-    }
-
-    private Map<String, Boolean> getPrefixSet(List<String> words) {
-        return null;
+        return x >= 0 && x < board.length && 
+                y >= 0 && y < board[x].length;
     }
 }
