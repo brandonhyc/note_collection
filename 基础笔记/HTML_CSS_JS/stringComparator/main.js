0ee960e16f5ea03ee9117dbcb05400b72ebb0008
Brandonhyc <brandon.heyc@gmail.com>
2018-10-09 23:02:08 -0400

    string

--- 基础笔记/HTML_CSS_JS/stringComparator/main.js
+++ 基础笔记/HTML_CSS_JS/stringComparator/main.js
@@ -1,4 +1,35 @@
 
+/**
+ * main entry: get two input
+ */
+
+(async function() {
+
+    var str1 = await getInput("string 1");
+    var str2 = await getInput("string 2");
+    var result = await compute(str1, str2);
+
+    document.querySelector("#display_p").innerHTML = `result: ${ result }`;
+}) ()
+
+async function compute(str1, str2) {
+    var arrayA = str1.split("");
+    for (let index in arrayA) {
+        if (str2.indexOf(arrayA[index]) !== -1) {
+            arrayA[index] = "_"
+        }
+    }
+    var result = arrayA.join("");
+    return result;
+}
+
+async function getInput(strName) {
+    var str1 = await prompt(`enter ${strName}`);
+    return str1;
+}
+
+
+
 
 function display() {
     var stringA = document.querySelector("#stringA").value;
