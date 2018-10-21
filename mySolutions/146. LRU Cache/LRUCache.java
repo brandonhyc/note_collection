a5149469f33a8dab4ab08005d6474aad7bc55681
Brandonhyc <brandon.heyc@gmail.com>
2018-10-20 22:47:37 -0400

    no.146

--- mySolutions/146. LRU Cache/LRUCache.java
+++ mySolutions/146. LRU Cache/LRUCache.java
@@ -1,94 +1,68 @@
+// 重点, 考察到 1.双向链的应用 2. 多线程
+
 public class LRUCache {
     /*
     * @param capacity: An integer
     */
-
-    class ListNode {
-        public int key, val;
-        public ListNode next;
-
-        public ListNode(int key, int val) {
+    class Node {
+        Node prev;
+        Node next;
+        int value;
+        int key;
+        public Node(int key, int value) {
             this.key = key;
-            this.val = val;
+            this.value = value;
+            this.prev = null;
             this.next = null;
         }
     }
 
-    private int capacity, size;
-    private ListNode dummy, tail;
-    private Map<Integer, ListNode> keyToPrev;
+    private int capacity;
+    private HashMap<Integer, Node> hs = new HashMap<Integer, Node>();
+    private Node head = new Node(-1, -1);;
+    private Node tail = new Node(-1, -1);;
 
     public LRUCache(int capacity) {
-        // do intialization if necessary
         this.capacity = capacity;
-        this.keyToPrev = new HashMap<Integer, ListNode>();
-        this.dummy = new ListNode(0, 0);
-        this.tail = this.dummy;
+        head.next = tail;
+        tail.prev = head;
     }
 
-    private void moveToTail(int key) {
-        ListNode prev = keyToPrev.get(key);
-        ListNode curt = prev.next;
-
-        if (tail == curt) {
-            return;
-        }
-        prev.next = prev.next.next;
-
-
-    }
-    /* get(key) 
-     *      Get the value (will always be positive) of 
-     *      the key if the key exists in the cache, otherwise return -1.
-     * @param key: An integer
-     * @return: An integer
-     * 
-     *
-     */
     public int get(int key) {
-        // write your code here
-        if (!keyToPrev.containsKey(key)) {
+        if (!hs.containsKey(key)) {
             return -1;
         }
 
-        moveToTail(key);
-        return tail.val;
+        Node current = hs.get(key);
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+
+        move_to_tail(current);
+        return current.value; 
     }
 
-    /* set(key, value) - 
-     *      Set or insert the value if the key is not already present. 
-     *      When the cache reached its capacity, 
-     *      it should invalidate the least recently used item before inserting a new item.
-     * @param key: An integer
-     * @param value: An integer
-     * @return: nothing
-     */
     public void set(int key, int value) {
-        // write your code here
         if (get(key) != -1) {
-            ListNode prev = keyToPrev.get(key);
-            prev.next.val = value;
+            hs.get(key).value = value;
             return;
         }
 
-        if (size < capacity) {
-            size++;
-            ListNode curt = new ListNode(key, value);
-            tail.next = curt;
-            keyToPrev.put(key, tail);
-
-            tail = curt;
-            return;
+        if (hs.size() == capacity) {
+            hs.remove(head.next.key);
+            head.next = head.next.next;
+            head.next.prev = head;
         }
-
-        ListNode first = dummy.next;
-        keyToPrev.remove(first.key);
-
-        first.key = key;
-        first.val = value;
-        keyToPrev.put(key, dummy);
-        
-        moveToTail(key);
-
+        Node insert = new Node(key, value);
+        hs.put(key, insert);
+        move_to_tail(insert);
     }
+
+    private void move_to_tail(Node current) {
+
+        current.prev = tail.prev;
+        current.next = tail;
+        tail.prev.next = current;
+        tail.prev = current;
+    }
+
 }
