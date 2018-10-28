5fc995b13f35d8c46937d4a7f503b58089938487
Brandonhyc <brandon.heyc@gmail.com>
2018-10-28 10:34:11 -0400

    added no.14

---
+++ mySolutions/14. (lint)First Position of Target/binarySearchFirstPosition.java
@@ -0,0 +1,71 @@
+public class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+
+    boolean test = false;
+    public int binarySearch(int[] nums, int target) {
+        // write your code here
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;
+        if (test) {
+            System.out.printf("%s, t: %d\n 0  1  2  3  4  5  6  7  8  9  10\n________________________________\nstart(i/v), mid, end\n",Arrays.toString(nums), target);
+        }
+        while (start + 1 < end) {
+            
+            int mid = start + (end - start) / 2;
+            if (test) {
+                binarySearchPrintHelper(nums, start, end, mid, target);
+            }
+
+            if (target == nums[mid]) {
+                end = mid;
+            }
+            if (target < nums[mid]) {
+                end = mid;
+            }
+            if (nums[mid] < target) {
+                start = mid;
+            }
+        }
+
+        if (nums[start] == target) {
+            return start;
+        }
+        if (nums[end] == target) {
+            return end;
+        }
+        return -1;
+    }
+
+
+
+    public void binarySearchPrintHelper(int[] nums, int start, int end, int mid, int target) {
+        // System.out.printf("start(i/v), mid, end\n%s\n", Arrays.toString(nums));
+
+        if (nums[mid] == target) {
+            System.out.printf("[%d]:%d|([%d]:%d|[%d]:%d), get mid, mid to right\n", 
+                                start, nums[start],
+                                mid, nums[mid],
+                                end, nums[end] );
+        }
+        if (nums[mid] < target) {
+            System.out.printf("([%d]:%d|[%d]:%d)| [%d]:%d, mid to right\n", 
+                                start, nums[start],
+                                mid, nums[mid],
+                                end, nums[end] );
+        }
+        if (nums[mid] > target) {
+            System.out.printf("[%d]:%d|([%d]:%d|[%d]:%d) left to mid\n", 
+                                start, nums[start],
+                                mid, nums[mid],
+                                end, nums[end] );
+        }
+
+    }
+}
---
+++ mySolutions/binarySearch/lastPosition.java
@@ -0,0 +1,70 @@
+public class Solution {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int lastPosition(int[] nums, int target) {
+        // write your code here
+// [1,2,2,4,5,5] 2
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int start = 0, end = nums.length - 1;// 2,3
+        // System.out.printf("%s, t: %d\n 0  1  2  3  4  5  6  7  8  9  10\n________________________________\nstart(i/v), mid, end\n",Arrays.toString(nums), target);
+        
+        while (start + 1 < end) {
+            
+            int mid = start + (end - start) / 2;//2
+
+            // binarySearchPrintHelper(nums, start, end, mid, target);
+
+            if (nums[mid] == target) { // 4 != 2 nums[mid] > target
+                start = mid;
+            }
+
+            if (target < nums[mid]) {
+                end = mid;
+            }
+            if (nums[mid] < target) {
+                start = mid;
+            }
+
+        }
+
+        if (nums[end] == target) {
+            return end;
+        }
+
+        if (nums[start] == target) {
+            return start;
+        }
+
+        return -1;
+    }
+
+    public void binarySearchPrintHelper(int[] nums, int start, int end, int mid, int target) {
+        // System.out.printf("start(i/v), mid, end\n%s\n", Arrays.toString(nums));
+
+        if (nums[mid] == target) {
+            System.out.printf("[%d]:%d|([%d]:%d|[%d]:%d), get mid, mid to right\n", 
+                                start, nums[start],
+                                mid, nums[mid],
+                                end, nums[end] );
+        }
+        if (nums[mid] < target) {
+            System.out.printf("([%d]:%d|[%d]:%d)| [%d]:%d, mid to right\n", 
+                                start, nums[start],
+                                mid, nums[mid],
+                                end, nums[end] );
+        }
+        if (nums[mid] > target) {
+            System.out.printf("[%d]:%d|([%d]:%d|[%d]:%d) left to mid\n", 
+                                start, nums[start],
+                                mid, nums[mid],
+                                end, nums[end] );
+        }
+
+    }
+}
