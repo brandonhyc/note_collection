f199f91a68a379ce32208f0a1d929b67aabbf702
Brandonhyc <brandon.heyc@gmail.com>
2018-11-01 21:49:03 -0400

    no.29

---
+++ mySolutions/29. Divide Two Integers/divide.java
@@ -0,0 +1,37 @@
+public class Solution {
+    /**
+     * @param dividend: the dividend
+     * @param divisor: the divisor
+     * @return: the result
+     */
+    public int divide(int dividend, int divisor) {
+        // write your code here
+        if (divisor == 0) {
+            return dividend >= 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        } 
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        boolean isNegative = (dividend > 0 && divisor < 0) ||
+                             (dividend < 0 && divisor > 0);
+        
+        long positiveDividend = Math.abs((long) dividend);
+        long positiveDivisor = Math.abs((long) divisor);
+
+        int result = 0;
+
+        while (positiveDividend >= positiveDivisor) {
+            int shiftTime = 0;
+            
+            while (positiveDividend >= (positiveDivisor << shiftTime)) {
+                shiftTime++;
+            }
+
+            positiveDividend -= positiveDivisor << (shiftTime -1);
+            result +=  1 << (shiftTime - 1);
+        }
+
+        return isNegative ? -result : result;
+    }
+}
