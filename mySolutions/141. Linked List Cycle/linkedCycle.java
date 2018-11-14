b1124abd3b2241c050a4bd2cfc00540b0816f72c
Brandonhyc <brandon.heyc@gmail.com>
2018-11-13 23:24:15 -0500

    no.141

---
+++ mySolutions/141. Linked List Cycle/linkedCycle.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for ListNode
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: True if it has a cycle, or false
+     */
+    public boolean hasCycle(ListNode head) {
+        // write your code here
+
+        if (head == null) {
+            return false;
+        }
+
+        Set<ListNode> set = new HashSet<>();
+        ListNode node = head;
+
+        while (node.next != null) {
+            if (!set.add(node)) {
+                node = node.next;
+                continue;
+            }
+            return true;
+        }
+
+        return false;
+
+    }
+}
