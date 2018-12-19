e3b5879fe29844259274546fc75260a3957df9ab
Brandonhyc <brandon.heyc@gmail.com>
2018-12-19 15:55:51 -0500

    no.286

--- mySolutions/286.Walls and Gates/286.663.wallsAndGates.java
+++ mySolutions/286.Walls and Gates/286.663.wallsAndGates.java
@@ -6,7 +6,61 @@ public class Solution {
     public void wallsAndGates(int[][] rooms) {
         // write your code here
 
+        if (rooms == null || rooms.length == 0 || rooms[0].length == 0) {
+            return;
+        }
+        boolean[][] visited = new boolean[rooms.length][rooms[0].length];
 
-        
+        List<int[]> doors = new ArrayList<>();
+        for (int x = 0; x < rooms.length; x++) {
+            for (int y = 0; y < rooms[0].length; y++) {
+                if (rooms[x][y] == 0) {
+                    doors.add(new int[]{x, y});
+                }
+            }
+        }
+        visited = new boolean[rooms.length][rooms[0].length];
+        bfs(rooms, visited, doors);
+    
+    }
+
+    private void bfs(int[][] rooms, boolean[][] visited, List<int[]> doors) {
+        int[] dx = {0, 1, 0, -1};
+        int[] dy = {1, 0, -1, 0};
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < doors.size(); i++) {
+            queue.offer(doors.get(i));
+        }
+
+        while (!queue.isEmpty()) {
+            int[] room = queue.poll();
+            int x = room[0];
+            int y = room[1];
+
+            if (visited[x][y]) {
+                continue;
+            }
+            visited[x][y] = true;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (!(0 <= nx && nx < rooms.length && 
+                      0 <= ny && ny < rooms[0].length)) {
+                    continue;
+                }
+                
+                if (rooms[nx][ny] == -1) {
+                    continue;
+                }
+                queue.offer(new int[]{nx, ny});
+
+                rooms[x][y] = rooms[nx][ny] + 1 < 0 ? rooms[x][y]:
+                                Math.min(rooms[x][y], rooms[nx][ny] + 1);
+            }
+        }
     }
 }
--- mySolutions/305.Number of Islands II/305.434.NumberOfIslandsII.java
+++ mySolutions/305.Number of Islands II/305.434.NumberOfIslandsII.java
@@ -18,13 +18,14 @@ public class Solution {
     public List<Integer> numIslands2(int n, int m, Point[] operators) {
         // write your code here
 
-        if (n == 0 || m == 0 || operators.length == 0) {
+        if (n == 0 || m == 0 || 
+            operators == null || operators.length == 0) {
             return new ArrayList<>();
         }
 
         int result = 0;
         List<Integer> results = new ArrayList<Integer>();
-        Set<List<Integer>> visited = new HashSet<>();
+        Map<Integer, Set<Integer>> visited = new HashMap<>();
         // 1. read operator in operators
         // 2. bfs, if visited contains return origin result;
         
@@ -36,39 +37,35 @@ public class Solution {
 
     }
 
-
-    private int bfs(int n, int m, Point operator, Set<List<Integer>> visited, int count) {
+    private int bfs(int n, int m, Point operator, Map<Integer, Set<Integer>> visited, int count) {
         
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
         
-        List<Integer> point = new ArrayList<>();
-        point.add(operator.x);
-        point.add(operator.y);
+        int x = operator.x;
+        int y = operator.y;
 
-        System.out.print("Point: ");
-        System.out.println(point);
+        if (!visited.containsKey(x)) {
+            Set set = new HashSet<>();
+            set.add(y);
+            visited.put(x, set);
+        }
+        visited.get(x).add(y);
 
         for (int i = 0; i < 4; i++) {
             int nx = operator.x + dx[i];
             int ny = operator.y + dy[i];
 
-            List<Integer> nextPoint = new ArrayList<>();
-            nextPoint.add(nx);
-            nextPoint.add(ny);
-
-            if (0 <= ny && ny < m &&
-                0 <= nx && nx < n &&
-                visited.contains(nextPoint)) {
-                
+            if (!(0 <= ny && ny < m &&
+                0 <= nx && nx < n)) {
                 continue;
             }
-                System.out.print("nextPoint: ");
-                System.out.println(nextPoint);
-         
-            count++;
-            break;
+            if (visited.containsKey(nx) &&
+                visited.get(nx).contains(ny)) {
+                return count;
+            }
         }
+        count++;
         return count;
     }
 }
