f6e73b0ba9d67c77ab2ea8e4f99c926d3675ffa4
yuchonghe <brandon.heyc@gmail.com>
2019-02-13 14:36:29 -0500

    no.587

---
+++ .history/mySolutions/532. Amazon.K-diff Pairs in an Array/532.pairTwoSum_20190213133531.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findPairs(int[] nums, int k) {
+        
+
+        
+
+    }
+}
---
+++ .history/mySolutions/587. (lint)Two Sum - Unique pairs/587.Amazon.twoSum-uniquePairs_20190213143454.java
@@ -0,0 +1,42 @@
+public class Solution {
+    /**
+     * @param nums: an array of integer
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int twoSum6(int[] nums, int target) {
+        // write your code here
+
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int result = 0;
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+
+
+            if (nums[left] + nums[right] == target) {
+                result++;
+                if (left < right && left + 1 < nums.length && nums[left] == nums[left + 1]) {
+                    left++;
+                }
+                if (left < right && right - 1 < nums.length && nums[right] == nums[right - 1]) {
+                    right--;
+                }
+                left++;
+                right--;
+            } 
+            else if (nums[left] + nums[right] < target) {
+                left++;
+            } 
+            else if (nums[left] + nums[right] > target) {
+                right--;
+            }
+        }
+        return result;
+    }
+}
