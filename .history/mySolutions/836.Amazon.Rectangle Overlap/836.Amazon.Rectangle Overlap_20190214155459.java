24ef553beaacc58df7d2e9333f3d90a9cf9ba841
yuchonghe <brandon.heyc@gmail.com>
2019-02-14 16:14:27 -0500

    no.836

---
+++ .history/mySolutions/836.Amazon.Rectangle Overlap/836.Amazon.Rectangle Overlap_20190214155459.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        
+        if (rect1[0] )
+
+    }
+}
---
+++ .history/mySolutions/836.Amazon.Rectangle Overlap/836.Amazon.Rectangle Overlap_20190214160315.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        boolean rec1OntheLeftOfrect2 = rec1[2] < rec2[0]; 
+        boolean rec1OntheRightOfrect2 = rec1[2] < rec2[0]; 
+        return 
+    }
+}
