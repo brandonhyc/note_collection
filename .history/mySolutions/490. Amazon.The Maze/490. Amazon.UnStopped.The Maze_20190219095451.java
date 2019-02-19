df3c5a506c3b5b55981a6d31e20dd440a049d1f1
yuchonghe <brandon.heyc@gmail.com>
2019-02-19 10:01:20 -0500

    no.490

---
+++ .history/mySolutions/490. Amazon.The Maze/490. Amazon.UnStopped.The Maze_20190219095451.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        
+        if (maze == null || maze.length == 0 || maze[0].length == 0) {
+            return false;
+        }
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        
+        return search(maze, start, destination, visited);
+
+    }
+
+    
+    private boolean search(int[][] maze, int[] start, int[] dest, boolean[][] visited) {
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0, 1, 0, -1};
+
+        if (visited[start[0]][start[1]]) {
+            return false;
+        }
+
+        boolean hasPath = false; 
+        // if not visited
+        visited[start[0]][start[1]] = true;
+        // exit
+        if (start[0] == dest[0] && start[1] == dest[1]) {
+            return true;
+        }
+        for (int i = 0; i < 4; i++) {
+            int[] nextStart = new int[]{start[0], start[1]};
+            while (true) {
+                nextStart = new int[]{start[0] + dx[i], start[1] + dy[i]};
+                if (nextStart[0] < 0 || nextStart[0] >= maze.length
+                    || nextStart[1] < 0 || nextStart[1] >= maze[0].length) {
+                    break;
+                }
+                if (maze[nextStart[0]][nextStart[1]] == 1) {
+                    break;
+                }
+            }
+            hasPath = hasPath || search(maze, nextStart, dest, visited);
+        }
+        return hasPath;
+    }
+}
---
+++ .history/mySolutions/490. Amazon.The Maze/490. Amazon.UnStopped.The Maze_20190219100024.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        
+        if (maze == null || maze.length == 0 || maze[0].length == 0) {
+            return false;
+        }
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        
+        return search(maze, start, destination, visited);
+
+    }
+
+    
+    private boolean search(int[][] maze, int[] start, int[] dest, boolean[][] visited) {
+        int[] dx = new int[]{1, 0, -1, 0};
+        int[] dy = new int[]{0, 1, 0, -1};
+
+        if (visited[start[0]][start[1]]) {
+            return false;
+        }
+
+        boolean hasPath = false; 
+        // if not visited
+        visited[start[0]][start[1]] = true;
+        // exit
+        if (start[0] == dest[0] && start[1] == dest[1]) {
+            return true;
+        }
+        for (int i = 0; i < 4; i++) {
+            int[] nextStart = new int[]{start[0], start[1]};
+            while (true) {
+                int nextX = nextStart[0] + dx[i];
+                int nextY = nextStart[0] + dy[i];
+                if (nextX < 0 || nextX >= maze.length
+                    || nextY < 0 || nextY >= maze[0].length) {
+                    break;
+                }
+                if (maze[nextX][nextY] == 1) {
+                    break;
+                }
+                nextStart = new int[]{nextX, nextY};
+            }
+            hasPath = hasPath || search(maze, nextStart, dest, visited);
+        }
+        return hasPath;
+    }
+}
