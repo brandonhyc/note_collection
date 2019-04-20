80d662bdafa4fbbd0df00eb4b91599fe7961f58a
yuchonghe <brandon.heyc@gmail.com>
2019-04-19 21:40:28 -0400

    added playground

---
+++ .history/playgroundA_20190419213937.c
@@ -0,0 +1,78 @@
+/* Michell Li   mli88@jhu.edu
+SCRAM Assembler
+asm.cpp (main)
+*/
+
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <sstream>
+#include "functions.h"
+
+using std::string;
+using std::cin;
+using std::cout;
+using std::ifstream;
+using std::endl;
+
+
+int main(int argc, char *argv[]) {
+  FILE* fp;
+  FILE* fw;
+  int imp = 0;
+
+  if (argc > 3) { //error
+    fprintf(stderr, "too many inputs\n");
+    return 1;
+  }
+
+  if (argc == 3) { //read file
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+      fprintf(stderr, "The input file cannot be opened or cannot be read.\n");
+      return 2;
+    }
+    fw = fopen(argv[2], "w");
+    if (fw == NULL) {
+      fprintf(stderr, "The output file cannot be opened or cannot be written.\n");
+      return 3; //output file cannot be opened/written
+    } else {
+      imp = implement(fp);
+      if (imp != 0) {
+        return imp;
+      } else {
+        return readLines(fw);
+      }
+    }
+  }
+
+  if (argc == 2) {
+    fp = fopen(argv[1], "r");
+    fw = stdout;
+    if (fp == NULL) {
+      fprintf(stderr, "The input file cannot be opened or cannot be read.\n");
+      return 2;
+    } else {
+      imp = implement(fp);
+      if (imp != 0) {
+        return imp;
+      } else {
+        return readLines(fw);
+      }
+    }
+  }
+
+  if (argc == 1) { //read from stdin
+    imp = implement(stdin);
+    if (imp != 0) {
+      return imp;
+    } else {
+      readLines(stdout);
+    }
+  }
+
+} //end of file
