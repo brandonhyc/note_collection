ddd666a2b0d19957b6af6f6d96e4cae54e44501f
He, Blake (NIH/OD) [C] <blake.he@nih.gov>
2019-08-14 17:54:04 -0400

    no.79 Word Search

--- mySolutions/79. Word Search/79. Word Search.java
+++ mySolutions/79. Word Search/79. Word Search.java
@@ -12,6 +12,7 @@ class Solution {
         boolean result = false;
         for (int x = 0; x < board.length; x++) {
             for (int y = 0; y < board[0].length; y++) {
+
                 if (board[x][y] == word.charAt(0)) {
                     result = dfs(board, word, 0, x, y);
                 }
@@ -22,23 +23,24 @@ class Solution {
 
     private boolean dfs(char[][] board, String word, int start, int ox, int oy) {
 
+        if (start == word.length() - 1 && board[ox][oy] == word.charAt(start)) {
+            System.out.printf("char: %s\n", word.charAt(start));
+            return true;
+        }
         boolean result = false;
+
         int[] dx = { 1, 0, -1, 0 };
         int[] dy = { 0, 1, 0, -1 };
+        System.out.printf("start char: %s, at: (%d, %d)\n", word.charAt(start), ox, oy);
         for (int i = 0; i < dx.length; i++) {
+
             int nx = ox + dx[i];
             int ny = oy + dy[i];
 
             if (!(0 <= nx && nx < board.length && 0 <= ny && ny < board[0].length)) {
-                return false;
+                continue;
             }
-
-            if (board[nx][ny] == word.charAt(start)) {
-
-                if (word.charAt(word.length() - 1) == board[nx][ny]) {
-                    return true;
-                }
-
+            if (board[nx][ny] == word.charAt(start + 1)) {
                 result = result || dfs(board, word, start + 1, nx, ny);
             }
         }
