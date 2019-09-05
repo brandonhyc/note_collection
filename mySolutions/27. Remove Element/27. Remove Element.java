df0b88c9fb9d4f80fc871390c1833492bd21832a
yuchonghe <brandon.heyc@gmail.com>
2019-09-04 22:54:19 -0400

    no.27 Remove Elment

--- mySolutions/27. Remove Element/27. Remove Element.java
+++ mySolutions/27. Remove Element/27. Remove Element.java
@@ -1,16 +1,17 @@
 class Solution {
-    public int removeElement(int[] nums, int val) {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
         
-        int slow = 0;
+        int slow = 1;
         
-        for (int fast = 0; fast < nums.length; fast++) {
-            if (nums[fast] != val) {
+        for (int fast = 1; fast < nums.length ; fast++) {
+            if (nums[fast] > nums[slow - 1]) {
                 nums[slow] = nums[fast];
                 slow++;
             }
-            
         }
-        
         return slow;
     }
 }
