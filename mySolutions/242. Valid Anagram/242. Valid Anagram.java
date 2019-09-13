c904492399527a1e630f61300bf281c946b53f3a
He, Blake (NIH/OD) [C] <blake.he@nih.gov>
2019-09-13 18:10:41 -0400

    49. Group Anagrams

--- mySolutions/242. Valid Anagram/242. Valid Anagram.java
+++ mySolutions/242. Valid Anagram/242. Valid Anagram.java
@@ -1,31 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if  (s == null && t == null) {
-            return true;
-        }
-        if (s == null || t == null || s.length() != t.length()) {
+        if (s.length() != t.length()) {
             return false;
         }
-        
-        Map<Integer, Integer> mapS = new HashMap<>();
-        Map<Integer, Integer> mapT = new HashMap<>();
-        
-        putInMap(mapS, s);
-        putInMap(mapT, t);
-        
-        return mapS.equals(mapT);
-    }
-    
-    private void putInMap(Map<Integer, Integer> map, String s) {
+
+        int[] charSArray = new int[26];
+
         for (int i = 0; i < s.length(); i++) {
-            Integer value = map.get(s.charAt(i) - 'a');
-            
-            if (value == null) {
-                map.put(s.charAt(i) - 'a', 0);
-                value = 0;
-            }
-            
-            map.put(s.charAt(i) - 'a', value + 1);
+            charSArray[s.charAt(i) - 'a']++;
+            charSArray[t.charAt(i) - 'a']--;
         }
+
+        for (int i = 0; i < charSArray.length; i++) {
+            if (charSArray[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
