6b3f9fbb3157d35c0dc003964647ff30f78ae60c
brandonhyc <brandon.heyc@gmail.com>
2019-09-15 17:20:47 -0400

    77. Combinations

---
+++ mySolutions/77. Combinations/77. Combinations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> results = new ArrayList<>();
+        dfs(n, k, results, new ArrayList<>(), 1);
+        
+        return results; 
+    }
+    
+    private void dfs(int n, int k, List<List<Integer>> results, List<Integer> combination, int start) {
+        
+        if (k == 0) {
+            results.add(new ArrayList<>(combination));
+            return;
+        }
+        if (start >= n + 1) {
+            return;
+        }
+        
+        combination.add(start);
+        dfs(n, k - 1, results, combination, start + 1);
+        combination.remove(combination.size() - 1);
+        
+        dfs(n, k, results, combination, start + 1);
+    }
+}
