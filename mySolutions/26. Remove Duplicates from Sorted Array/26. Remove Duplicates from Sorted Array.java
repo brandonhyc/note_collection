1097b3de7e44d70e7da4399c69bc9462587c25be
He, Blake (NIH/OD) [C] <blake.he@nih.gov>
2019-09-17 16:04:15 -0400

    26. Remove Duplicates from Sorted Array

---
+++ mySolutions/26. Remove Duplicates from Sorted Array/26. Remove Duplicates from Sorted Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int slow = 1;
+        // [0,0,1,1,1,2,2,3,3,4]
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+
+        return slow;
+    }
+}
