59b0601c71a6f351b9ba2de019c42f89ab4c1b46
He, Blake (NIH/OD) [C] <blake.he@nih.gov>
2019-09-24 17:59:58 -0400

    update

--- mySolutions/200. Number of Islands/200. Number of Islands.2.java
+++ mySolutions/200. Number of Islands/200. Number of Islands.2.java
@@ -8,13 +8,12 @@ class Solution {
 
         boolean[][] visited = new boolean[grid.length][grid[0].length];
 
-        for (int y = 0; y < grid[0].length; y++) {
+        for (int y = 0; y < grid.length; y++) {
 
-            for (int x = 0; x < grid.length; x++) {
-                if (!visited[x][y] && grid[x][y] == '1') {
-                    // System.out.printf("x: %d, y: %d\n", x, y);
+            for (int x = 0; x < grid[0].length; x++) {
+                if (grid[y][x] == '1') {
                     result++;
-                    dfs(grid, visited, x, y);
+                    dfs(grid, x, y);
                 }
             }
         }
@@ -22,26 +21,24 @@ class Solution {
         return result;
     }
 
-    private void dfs(char[][] grid, boolean[][] visited, int x, int y) {
+    private void dfs(char[][] grid, int x, int y) {
         int[] dx = { 1, 0, -1, 0 };
         int[] dy = { 0, -1, 0, 1 };
 
-        if (visited[x][y]) {
-            return;
-        } else
-            visited[x][y] = true;
+        grid[y][x] = '0';
 
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
 
-            if (!((nx >= 0 && nx < visited.length) && (ny >= 0 && ny < visited[0].length))) {
+            if (!((nx >= 0 && nx < grid[0].length) && (ny >= 0 && ny < grid.length))) {
                 continue;
             }
-            if (grid[x][y] != '1') {
+            if (grid[ny][nx] != '1') {
                 continue;
             }
-            dfs(grid, visited, nx, ny);
+
+            dfs(grid, nx, ny);
         }
 
     }
