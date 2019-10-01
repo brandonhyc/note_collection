47a72839aab811595b3e0be2f76dfb3451a5f5fa
He, Blake (NIH/OD) [C] <blake.he@nih.gov>
2019-10-01 16:31:58 -0400

    388. Longest Absolute File Path

--- mySolutions/71. Simplify Path/71. Simplify Path.java
+++ mySolutions/71. Simplify Path/71. Simplify Path.java
@@ -1,35 +1,26 @@
 class Solution {
-    public String simplifyPath(String path) {
-        // "/a/./b/../../c/"
-        // "/a/../../b/../c//.//"
-        Stack<String> stack = new Stack<>();
-        int leftSlash = 0;
-        while (leftSlash < path.length()) {
-            int rightSlash = leftSlash + 1;
-            while (rightSlash < path.length() && path.charAt(rightSlash) != '/') {
-                rightSlash++;
-            }
-            String clip = path.substring(leftSlash + 1, rightSlash);
-            if (clip.length() == 0 || clip.equals(".")) {
-                // doing nothing
-            } else if (clip.equals("..")) {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else {
-                // normal
-                stack.push(clip);
-            }
-            leftSlash = rightSlash;
-        }
+    public int lengthLongestPath(String input) {
 
-        Collections.reverse(stack);
-        StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append("/");
-            sb.append(stack.pop());
+        Stack<Integer> stack = new Stack<>();
+        int max = 0;
+        String[] lines = input.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            // dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext
+            String line = lines[i];
+            int tapCount = line.lastIndexOf("\t") + 1;
 
+            while (tapCount < stack.size()) {
+                stack.pop();
+            }
+            int pathLength = (stack.isEmpty() ? 0 : stack.peek()) + line.length() - tapCount;
+            // System.out.printf("line: %s, tapCount: %d, pathLength: %d\n", line, tapCount,
+            // pathLength);
+            stack.push(pathLength + "/".length());
+
+            if (line.indexOf(".") != -1) {
+                max = Math.max(pathLength, max);
+            }
         }
-        return sb.length() != 0 ? sb.toString() : "/";
+        return max;
     }
 }
