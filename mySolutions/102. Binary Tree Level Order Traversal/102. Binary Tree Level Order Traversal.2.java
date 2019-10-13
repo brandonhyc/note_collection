db239a696efea8224a1873e213d71fdbf76cd051
brandonhyc <brandon.heyc@gmail.com>
2019-10-13 13:14:04 -0400

    102. Binary Tree Level Order Traversal

---
+++ mySolutions/102. Binary Tree Level Order Traversal/102. Binary Tree Level Order Traversal.2.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> results = new ArrayList<>();
+        
+        if (root == null) {
+            return results;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i ++) {
+                root = queue.poll();
+                level.add(root.val);
+                if (root.left != null) {
+                    queue.offer(root.left);
+                }
+                if (root.right != null) {
+                    queue.offer(root.right);
+                }
+                
+            }
+            results.add(level);
+        }
+        
+        return results;
+        
+        
+    }
+}
