7b6bbdcba0dc16edbfd3f239bdc61fabd9a9f819
brandonhyc <brandon.heyc@gmail.com>
2019-10-14 13:05:14 -0400

    199. Binary Tree Right Side View

--- mySolutions/103. Binary Tree Zigzag Level Order Traversal/103. Binary Tree Zigzag Level Order Traversal.2.java
+++ mySolutions/103. Binary Tree Zigzag Level Order Traversal/103. Binary Tree Zigzag Level Order Traversal.2.java
@@ -49,3 +49,34 @@ class Solution {
         return levels;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderRightToLeft(root, result, 0);
+        return result;
+    }
+    
+    private void preorderRightToLeft(TreeNode root, List<Integer> order, int depth) {
+        if (root == null) {
+            return;
+        }
+        
+        if (order.size() <= depth) {
+            order.add(root.val);
+        }
+        
+        preorderRightToLeft(root.right, order, depth + 1);
+        preorderRightToLeft(root.left, order, depth + 1);
+        
+    }
+}
