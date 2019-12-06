5bad8882ba63123e9a5b94c462dc72afaba370ec
brandonhyc <brandon.heyc@gmail.com>
2019-12-05 20:11:52 -0500

    509. Fibonacci Number

--- mySolutions/509. Fibonacci Number/509. Fibonacci Number.java
+++ mySolutions/509. Fibonacci Number/509. Fibonacci Number.java
@@ -1,5 +1,24 @@
 class Solution {
     public int fib(int N) {
+        if (N <= 1) {
+            return N;
+        }        
+
+        int n1 = 0; int n2 = 1;
+
+        for (int i = 2; i <= N; i++) {
+            int temp = n1 + n2;
+            n1 = n2;
+            n2 = temp;
+            
+        }
+
+        return n2;
+    }
+}
+
+class Solution {
+    public int fib(int N) {
         Integer[] dp = new Integer[N + 1];
         
         for (int i = 0; i <= N; i++) {
