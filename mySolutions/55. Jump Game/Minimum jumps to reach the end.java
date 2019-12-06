70b233d710269816265208f4d17030a926c3ba12
He, Blake (NIH/OD) [C] <blake.he@nih.gov>
2019-12-06 16:04:24 -0500

    Minimum jumps to reach the end

--- mySolutions/55. Jump Game/Minimum jumps to reach the end.java
+++ mySolutions/55. Jump Game/Minimum jumps to reach the end.java
@@ -38,6 +38,45 @@ class ArrayJump {
         if (jumps.length == 0) {
             return 0;
         }
+
+        int[] dp = new int[jumps.length];
+
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i] == Integer.MAX_VALUE) {
+                continue;
+            }
+
+            for (int j = 1; j <= jumps[i]; j++) {
+                // pick the possible minimum move
+                if (i + j <= jumps.length - 1) {
+                    dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+                }
+            }
+        }
+
+        return (dp[jumps.length - 1] == Integer.MAX_VALUE) ? -1 : dp[jumps.length - 1];
+    }
+
+    public static void main(String[] args) {
+        ArrayJump aj = new ArrayJump();
+        int[] jumps = { 2, 1, 1, 1, 4 };
+        System.out.println(aj.countMinJumps(jumps));
+        jumps = new int[] { 1, 1, 3, 6, 9, 3, 0, 1, 3 };
+        System.out.println(aj.countMinJumps(jumps));
+    }
+}
+
+
+class ArrayJump {
+
+    public int countMinJumps(int[] jumps) {
+        if (jumps.length == 0) {
+            return 0;
+        }
         int[] dp = new int[jumps.length];
         for (int i = 0; i < dp.length; i++) {
             dp[i] = Integer.MAX_VALUE;
