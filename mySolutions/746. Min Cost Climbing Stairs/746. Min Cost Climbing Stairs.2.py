e2444a3418286f794e3d879b1638fbda9543bb6b
brandonhyc <brandon.heyc@gmail.com>
2019-12-06 20:09:17 -0500

    746. Min Cost Climbing Stairs

--- mySolutions/746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.2.py
+++ mySolutions/746. Min Cost Climbing Stairs/746. Min Cost Climbing Stairs.2.py
@@ -5,6 +5,26 @@ class Solution:
          # 2. create a new set take two steps if not excess, add the fee, recursively process the rest
         # return the set with minmum sum
         
+        if len(cost) <= 2:
+            return 0
+
+        dp = [sys.maxsize for i in range(len(cost))]
+
+        for i in range(0, len(cost)):
+            if i <= 1:
+                dp[i] = cost[i]
+                continue
+            dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+            
+        return min(dp[-1], dp[-2])
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # for each index
+         # 1. create a new set take one step if not excess, add the fee, recursively process the rest
+         # 2. create a new set take two steps if not excess, add the fee, recursively process the rest
+        # return the set with minmum sum
+        
         dp = [sys.maxsize for i in range(len(cost))]
 
         return min(self.minCostRec(cost, 0, dp), self.minCostRec(cost, 1, dp)) 
