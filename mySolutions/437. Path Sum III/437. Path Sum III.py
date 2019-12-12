317035076132a8294d24ad7c68065086379632ec
brandonhyc <brandon.heyc@gmail.com>
2019-12-11 20:34:58 -0500

    437. Path Sum III

--- mySolutions/437. Path Sum III/437. Path Sum III.py
+++ mySolutions/437. Path Sum III/437. Path Sum III.py
@@ -1,3 +1,4 @@
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -36,4 +37,43 @@ class Solution:
         for i in range(len(sums)):
             sums[i] -= root.val
         sums.pop()
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> int:
+        
+        if root == None:
+            return 0
+        
+        count1 = self.pathSum(root.left, sum)
+        count2 = self.pathSum(root.right, sum)
+        
+        count3 = self.countPathSumFrom(root, sum)
+        return count1 + count2 + count3
+    
+    def countPathSumFrom(self, root, sum):
+        
+        if root == None:
+            return 0
+        
+        sum -= root.val
+        
+        count1 = 0
+        count2 = 0
+        count3 = 0
+        
+        if sum == 0:
+            count1 += 1
+        
+        count2 = self.countPathSumFrom(root.left, sum)
+        count3 = self.countPathSumFrom(root.right, sum)
+        
+        return count1 + count2 + count3
+        
         
